0afb9a6e667ff26f9e32b116afa4a357e155f3b5
Kanyinsola Fapohunda Oyindamola <kfapohunda@teamapt.com>
2019-08-03 20:00:09 +0100

    Add TermsAndConditionsViewController

--- AgroPartnerships/TermsAndConditionsViewController.swift
+++ AgroPartnerships/TermsAndConditionsViewController.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2019 AgroPartnerships. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TermsAndConditionsViewController : UIViewController {
+    
+    @IBOutlet weak var pageSwitch: UISegmentedControl!
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func userChangedPageSegmentedControl(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0: contentTextView.text = "Page 1"
+        case 1: contentTextView.text = "Page 2"
+        case 2: contentTextView.text = "Page 3"
+
+        default:
+            break
+        }
+    }
+}
+
